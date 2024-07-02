2041396271ae747f716efb22eb5daba6b49ee964
kiran <ghalekiran03@gmail.com>
2024-07-02 21:56:02 +0900

    created packages

---
+++ src/test/java/com/example/cricketInformer/CricketInformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cricketInformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketInformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
